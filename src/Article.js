ce6cd0108d72a34799f2046270e94e27ee86d0c5
nikulio <sn97@icloud.com>
2017-10-24 09:58:26 +0200

    First commit. Basics of hide-show, changes states and passing props

---
+++ src/Article.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+
+// export default function Article(props) {
+// 	const {article} = props;
+// 	return (
+// 		<div>
+// 			<h3 className="title">{article.title}</h3>
+// 			<section className="text">{article.text}</section>
+// 		</div>
+// 	)
+// }
+
+export default class Article extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isOpen: false
+		}
+		// this.toggleOpen = this.toggleOpen.bind(this) - alternative to arrow bind
+	}
+
+	toggleOpen = () => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	};
+
+	getBody = () => {
+		if (!this.state.isOpen) return null;
+		const {article} = this.props;
+		return <section className="text">{article.text}</section>
+
+	};
+
+	render() {
+		const {article} = this.props;
+		const {isOpen} = this.state;
+		return (
+			<div>
+				<h3 className="title">{article.title}</h3>
+				<button onClick={this.toggleOpen}>
+					{isOpen ? 'close':'open'}
+				</button>
+				{this.getBody()}
+			</div>
+		)
+	}
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {render} from 'react-dom'
+import Article from './Article'
+import {articles} from "./fixtures";
+
+
+render (<Article article={articles[0]}/>, document.getElementById('container'));
