67f6b1249e27f5a00dd86f22313a7a930191c154
nikulio <sn97@icloud.com>
2017-10-25 14:49:17 +0200

    made stateless compotent

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -2,33 +2,26 @@ import React, {Component} from 'react'
 import Comment from './Comment'
 import toggleOpen from '../decorators/toggleOpen'
 
-class CommentList extends Component {
-	static defaultProps = {
-		comments: []
-	};
+function getBody({comments, isOpen}) {
+	if (!isOpen) return null;
 
-	getBody = () => {
-		const {comments, isOpen} = this.props;
-		if (!isOpen) return null;
+	if (!comments.length) return <p>no comments yet</p>;
+	return (
+		<ul>
+			{comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
+		</ul>
+	)
+};
 
-		if (!comments.length) return <p>no comments yet</p>;
-		return (
-			<ul>
-				{comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
-			</ul>
-		)
-	};
-
-	render() {
-		const {isOpen, toggleOpen} = this.props
-		const text = isOpen ? 'hide comments' : 'show comments';
-		return (
-			<div className="comments">
-				<button onClick={toggleOpen}>{text}</button>
-				{this.getBody()}
-			</div>
-		)
-	}
+function CommentList({comments = [], isOpen, toggleOpen}) {
+	const text = isOpen ? 'hide comments' : 'show comments';
+	return (
+		<div className="comments">
+			<button onClick={toggleOpen}>{text}</button>
+			{getBody({comments, isOpen})}
+		</div>
+	)
 }
+
 
 export default toggleOpen(CommentList)
