bb5d23ebafccaf9a8d4812c7fe3b171c880dc8b9
nikulio <sn97@icloud.com>
2017-10-27 00:33:50 +0200

    added select component

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,33 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import ArticleList from './ArticleList'
 import UserForm from './UserForm'
+import Select from 'react-select'
+import 'react-select/dist/react-select.css'
 
 class App extends Component {
 	static propTypes = {
 		articles: PropTypes.array.isRequired
 	};
 
+	state = {
+		selection: null,
 
+	}
 
+	changeSelection = (selection) => {
+		this.setState({selection})
+	}
 
 	render() {
+		const options = this.props.articles.map(article => ({
+			label: article.title,
+			value: article.id
+		}))
+
 		return (
 			<div>
-				<UserForm />
+				<UserForm/>
+				<Select options={options} value={this.state.selection} onChange={this.changeSelection} multi/>
 				<ArticleList articles={this.props.articles}/>
 			</div>
 		)
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -10,6 +10,7 @@ class UserForm extends Component {
 	}
 
 	handleUserChange = (ev) => {
+		if (ev.target.value.length > 10) return
 		this.setState({
 			username: ev.target.value
 		})
